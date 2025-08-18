1d52704948e57d6b8f2561fd32b7332b31417c4e
polipon <acer5970039@gmail.com>
2025-08-18 17:38:35 +0300

    migration date

--- server/db/migrations/20250812181439-create-card.js
+++ server/db/migrations/20250812181439-create-card.js
@@ -14,10 +14,12 @@ module.exports = {
       },
       createdAt: {
         allowNull: false,
+        defaultValue: new Date(),
         type: Sequelize.DATE
       },
       updatedAt: {
         allowNull: false,
+        defaultValue: new Date(),
         type: Sequelize.DATE
       },
       onDelete: 'cascade',
